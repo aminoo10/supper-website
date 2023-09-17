ba6e2c6213afa7d17f80b8be9fc5f089aaa5e981
ashcon minoo <acminoo@gmail.com>
2023-09-16 22:50:18 -0400

    added blog styling. strapi media library broken.

--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+
 
 
 export default function Gallery() {
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useQuery, gql } from '@apollo/client'
+import ReactMarkdown from 'react-markdown'
+
 
 const POSTS = gql`
     query GetPosts {
@@ -9,6 +11,7 @@ const POSTS = gql`
             attributes {
             title,
             body,
+            createdAt,
             media {
                 data {
                 attributes {
@@ -22,24 +25,40 @@ const POSTS = gql`
         }
     }
 `
+const convertISOToDate = (isoDate) => {
+    const options = { year: 'numeric', 
+                    month: 'long', 
+                    day: 'numeric', 
+                    hour: 'numeric', 
+                    minute: 'numeric', 
+                    hour12: true};
+
+    const date = new Date(isoDate);
+    return date.toLocaleString('en-US', options);
+}
+
 const url = process.env.REACT_APP_URL;
 
 export default function Homepage() {
     const { loading, error, data } = useQuery(POSTS)
 
     if (loading) return <p>Loading...</p>
-    if (error == []) return <p>Error :(</p>
+    if (error) return <p>Error :(</p>
 
     console.log(data);
     
   return (
-    <div>
+    <div className="blog">
         {data.posts.data.map(post => (
             <div key={post.id} className="post-card">
                 <h2>{post.attributes.title}</h2>
-                <img src={`${url}${post.attributes.media.data.attributes.formats.small.url}`}
-                     alt={post.attributes.media.data.attributes.name} />
-                <p>{post.attributes.body}</p>
+                <div className="inner-box">
+                    <p className="date-created">Posted on {convertISOToDate(post.attributes.createdAt)}</p>
+                    <img src={`${url}${post.attributes.media.data.attributes.formats.small.url}`}
+                        alt={post.attributes.media.data.attributes.name} />
+                    {/*<p>{post.attributes.body}</p>*/}
+                    <ReactMarkdown>{post.attributes.body}</ReactMarkdown>
+                </div>
             </div>
         ))}
     </div>
